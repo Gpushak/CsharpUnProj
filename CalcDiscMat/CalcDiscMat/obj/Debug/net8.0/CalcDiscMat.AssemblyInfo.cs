06e152d1cbe59692a283b81ab58cfaa291d9d195
Gleb Menov <menovgleb10@gmail.com>
2024-10-25 17:26:09 +0500

    he

--- CalcDiscMat/CalcDiscMat/obj/Debug/net8.0/CalcDiscMat.AssemblyInfo.cs
+++ CalcDiscMat/CalcDiscMat/obj/Debug/net8.0/CalcDiscMat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalcDiscMat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe70c70ab0befa938af8a462225b7a29c5b42e02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9caf30d4330f05355347da7c9a45b8b5ccea775")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalcDiscMat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalcDiscMat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe70c70ab0befa938af8a462225b7a29c5b42e02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9caf30d4330f05355347da7c9a45b8b5ccea775")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
